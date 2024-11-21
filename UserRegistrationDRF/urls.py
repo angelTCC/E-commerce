96e6ac79b62f0b8c6c8fe67ba5952b00a70d8a1c
angel <angel20170238b@gmail.com>
2024-11-21 17:09:57 -0500

    commit

--- UserRegistrationDRF/urls.py
+++ UserRegistrationDRF/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegistrationViewDRF, register_view
+from .views import UserRegistrationViewDRF, register_view, close_session
 
 urlpatterns = [
     path('register/', register_view, name='register'),  # This will render the registration form
     path('api/register/', UserRegistrationViewDRF.as_view(), name='user-register'),  # API for registration
+    path('close_session/', close_session, name='close_session')
 ]
--- UserRegistrationDRF/views.py
+++ UserRegistrationDRF/views.py
@@ -1,20 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.views.generic import View
 
 # Create your views here.
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
 from .serializers import UserRegistrationSerializerDRF
 
 class UserRegistrationViewDRF(APIView):
     def post(self, request, *args, **kwargs):
         serializer = UserRegistrationSerializerDRF(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            user = serializer.save()
+            login(request, user)
             return Response({'message': 'User registered successfully!'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 # views.py
 def register_view(request):
     return render(request, 'register.html')
+
+
+def close_session(request):
+    logout(request)
+    return render(request, 'home.html')
